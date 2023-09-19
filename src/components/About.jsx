c8e7e6feb6d2a09c082b08d40a08d7191119ee23
Haseebsheikh786 <125917762+Haseebsheikh786@users.noreply.github.com>
2023-09-19 23:53:52 +0500

    Init the project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,25 @@
 import React from "react";
+import Style from "./Style.module.css";
 
 const About = () => {
   return (
     <>
       <section id="about">
         <h2>About Me</h2>
-        <p>
+        <p class="shadow p-3 bg-body-tertiary rounded">
           I'm Haseeb Farrukh, a MERN Stack Developer with a strong foundation in
           Reactjs ,Nodejs, Expressjs and MongoDB. Eager to learn and contribute,
           I am passionate about creating dynamic and user-friendly websites.
-         </p>
-        <div className="my-3">
-          <h4>Skills:</h4>
-          <ul>
-            <li>HTML , CSS , JavaScript</li>
-            <li>Bootstrap , Tailwind CSS</li>
-            <li>Reactjs , Redux</li>
-            <li>Node.js , Express.js , MongoDB</li>
-            <li>Git & GitHub</li>
-            <li>Photography & Video Editing</li>
+        </p>
+        <h4 className="text-danger fw-bold mt-4">Skills:</h4>
+        <div id={Style.id} class="card my-2">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">HTML , CSS , JavaScript</li>
+            <li class="list-group-item">Bootstrap , Tailwind CSS</li>
+            <li class="list-group-item">Reactjs , Redux</li>
+            <li class="list-group-item">Node.js , Express.js , MongoDB</li>
+            <li class="list-group-item">Git & GitHub</li>
+            <li class="list-group-item">Photography & Video Editing</li>
           </ul>
         </div>
       </section>
